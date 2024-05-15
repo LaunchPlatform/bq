4bf35e7f1fc0bc0ba67a5281dc01f86a5aefd2fb
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-15 10:22:47 -0700

    Add process task

--- bq/processors/registry.py
+++ bq/processors/registry.py
@@ -1,4 +1,6 @@
 import collections
+import dataclasses
+import inspect
 import logging
 import typing
 
@@ -10,11 +12,39 @@ from bq import models
 BQ_PROCESSOR_CATEGORY = "bq_processor"
 
 
-class Processor(typing.NamedTuple):
+@dataclasses.dataclass(frozen=True)
+class Processor:
     module: str
     name: str
     channel: str
     func: typing.Callable
+    # should we auto complete the task or not
+    auto_complete: bool = True
+
+
+def process_task(task: models.Task, processor: Processor):
+    logger = logging.getLogger(__name__)
+    db = object_session(task)
+    func_signature = inspect.signature(processor.func)
+    base_kwargs = {}
+    if "task" in func_signature.parameters:
+        base_kwargs["task"] = task
+    if "db" in func_signature.parameters:
+        base_kwargs["db"] = db
+    try:
+        result = processor.func(**base_kwargs, **task.kwargs)
+        if processor.auto_complete:
+            logger.info("Task %s auto complete", task.id)
+            task.state = models.TaskState.DONE
+            db.add(task)
+            db.commit()
+        return result
+    except Exception:
+        logger.error("Unprocessed error for task %s", task.id, exc_info=True)
+        # TODO: add error event
+        task.state = models.TaskState.FAILED
+        db.add(task)
+        db.commit()
 
 
 class Registry:
@@ -28,9 +58,9 @@ class Registry:
     def process(self, task: models.Task) -> typing.Any:
         modules = self.processors.get(task.channel, {})
         functions = modules.get(task.module, {})
-        func = functions.get(task.func_name)
+        processor = functions.get(task.func_name)
         db = object_session(task)
-        if func is None:
+        if processor is None:
             self.logger.error(
                 "Cannot find processor for task %s with func=%s, module=%s",
                 task.id,
@@ -42,21 +72,18 @@ class Registry:
             db.add(task)
             db.commit()
             return
-        try:
-            return func(db=db, task=task, **task.kwargs)
-        except Exception:
-            self.logger.error("Unprocessed error for task %s", task.id, exc_info=True)
-            # TODO: add error event
-            task.state = models.TaskState.FAILED
-            db.add(task)
-            db.commit()
+        return process_task(task, processor)
 
 
-def processor(channel: str) -> typing.Callable:
+def processor(channel: str, auto_complete: bool = True) -> typing.Callable:
     def decorator(wrapped: typing.Callable):
         def callback(scanner: venusian.Scanner, name: str, ob: typing.Callable):
             processor = Processor(
-                module=ob.__module__, name=name, channel=channel, func=ob
+                module=ob.__module__,
+                name=name,
+                channel=channel,
+                func=ob,
+                auto_complete=auto_complete,
             )
             scanner.registry.add(processor)
 
