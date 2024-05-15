293f6fd24418671573fa1a835ec9ec93f68736b5
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-15 10:41:37 -0700

    Add tests

--- tests/unit/test_processor_registry.py
+++ tests/unit/test_processor_registry.py
@@ -1,4 +1,12 @@
+import typing
+
+import pytest
+from sqlalchemy.orm import Session
+
+from bq import models
 from bq.processors.registry import collect
+from bq.processors.registry import process_task
+from bq.processors.registry import Processor
 
 
 def test_collect():
@@ -17,3 +25,22 @@ def test_collect():
     assert modules1.keys() == {module_name}
     funcs1 = modules1[module_name]
     assert funcs1.keys() == {"processor1"}
+
+
+@pytest.mark.parametrize(
+    "func, expected",
+    [
+        (lambda: [], []),
+        (lambda task: {"task"}, ["task"]),
+        (lambda task, db: {"task", "db"}, ["task", "db"]),
+    ],
+)
+def test_process_task_kwargs(
+    db: Session, task: models.Task, func: typing.Callable, expected: list
+):
+    processor = Processor(
+        channel="mock-channel", module="mock.module", name="my_func", func=func
+    )
+    assert frozenset(process_task(task=task, processor=processor)) == frozenset(
+        expected
+    )
