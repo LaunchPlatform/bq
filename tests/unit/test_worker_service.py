d9b804b4bb8a52d36abb1249fa61f0489abe3fd7
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-16 12:19:32 -0700

    Add tests for reschedule

--- tests/unit/test_worker_service.py
+++ tests/unit/test_worker_service.py
@@ -4,6 +4,7 @@ import pytest
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
+from ..factories import TaskFactory
 from ..factories import WorkerFactory
 from bq import models
 from bq.services.worker import WorkerService
@@ -43,3 +44,40 @@ def test_fetch_dead_workers(
     assert alive_worker0.state == models.WorkerState.RUNNING
     assert alive_worker1.state == models.WorkerState.RUNNING
     assert alive_worker2.state == models.WorkerState.RUNNING
+    db.commit()
+
+
+def test_reschedule_dead_tasks(
+    db: Session,
+    worker_service: WorkerService,
+    worker_factory: WorkerFactory,
+    task_factory: TaskFactory,
+):
+    now = db.scalar(func.now())
+
+    dead_worker0 = worker_factory(last_heartbeat=now - datetime.timedelta(seconds=6))
+    dead_task0 = task_factory(worker=dead_worker0, state=models.TaskState.PROCESSING)
+    dead_task1 = task_factory(worker=dead_worker0, state=models.TaskState.PROCESSING)
+    done_task0 = task_factory(worker=dead_worker0, state=models.TaskState.DONE)
+
+    dead_worker1 = worker_factory(last_heartbeat=now - datetime.timedelta(seconds=7))
+    dead_task2 = task_factory(worker=dead_worker1, state=models.TaskState.PROCESSING)
+
+    alive_worker0 = worker_factory(last_heartbeat=now - datetime.timedelta(seconds=4))
+    other_task0 = task_factory(worker=alive_worker0, state=models.TaskState.PROCESSING)
+    alive_worker1 = worker_factory()
+    other_task1 = task_factory(worker=alive_worker1, state=models.TaskState.PROCESSING)
+
+    task_count = worker_service.reschedule_dead_tasks(
+        db.query(models.Worker.id).filter(
+            models.Worker.id.in_([dead_worker0.id, dead_worker1.id])
+        )
+    )
+    assert task_count == 3
+    db.commit()
+    assert dead_task0.state == models.TaskState.PENDING
+    assert dead_task1.state == models.TaskState.PENDING
+    assert dead_task2.state == models.TaskState.PENDING
+    assert done_task0.state == models.TaskState.DONE
+    assert other_task0.state == models.TaskState.PROCESSING
+    assert other_task1.state == models.TaskState.PROCESSING
