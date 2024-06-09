e585a9ab7bd83af18dd8bc5a5187fe12a5509632
Fang-Pen Lin <hello@fangpenlin.com>
2024-06-09 16:22:01 -0700

    Add test for attempt limit

--- tests/unit/processors/test_retry_policies.py
+++ tests/unit/processors/test_retry_policies.py
@@ -9,6 +9,7 @@ from ...factories import EventFactory
 from bq import models
 from bq.processors.retry_policies import Delay
 from bq.processors.retry_policies import ExponentialBackoff
+from bq.processors.retry_policies import LimitAttempt
 
 
 @pytest.mark.parametrize("failure_count", [0, 1, 5, 10])
@@ -49,3 +50,30 @@ def test_exponential_backoff(
     )
     actual = db.scalar(select(scheduled_at))
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "failure_count, expected_retry",
+    [
+        (0, True),
+        (1, True),
+        (5, False),
+        (6, False),
+        (7, False),
+    ],
+)
+def test_limit_attempt(
+    db: Session,
+    event_factory: EventFactory,
+    task: models.Task,
+    failure_count: int,
+    expected_retry: bool,
+):
+    for _ in range(failure_count):
+        event_factory(task=task, type=models.EventType.FAILED_RETRY_SCHEDULED)
+    policy = LimitAttempt(6, Delay(delay=datetime.timedelta(seconds=5)))
+    scheduled_at = policy(task)
+    if expected_retry:
+        assert scheduled_at is not None
+    else:
+        assert scheduled_at is None
