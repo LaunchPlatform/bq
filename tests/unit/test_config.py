3fee7b8a68eaef3687a31937eae15e947a3bd982
Fang-Pen Lin <hello@fangpenlin.com>
2024-12-24 18:10:11 -0800

    Add more tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,6 +2,7 @@ import typing
 
 import pytest
 from pydantic import PostgresDsn
+from pydantic import ValidationError
 
 from bq.config import Config
 
@@ -22,3 +23,18 @@ from bq.config import Config
 )
 def test_database_url(value: typing.Any, expected: PostgresDsn):
     assert Config(DATABASE_URL=value).DATABASE_URL == expected
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        1234,
+        12.34,
+        object(),
+        list(),
+        dict(),
+    ],
+)
+def test_bad_database_url_type(value: typing.Any):
+    with pytest.raises(ValidationError):
+        Config(DATABASE_URL=value)
